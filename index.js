d94e64376c36fd5ce61225a50730d9854b689398
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2025-03-10 17:10:21 -0600

    login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import indexRouter from './rutas/index.js';
 import { connectDB } from "./utils/sql.js";
 import userRoutes from "./rutas/rutas.js";
+import loginRoutes from "./rutas/login.js";
 
 const app = express();
 const port = 5000;
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use(indexRouter);
 app.use(userRoutes);
+app.use(loginRoutes);
 
 
 app.listen(port, console.log("http://localhost:"+ port));
