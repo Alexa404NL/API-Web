018fcc0e2b4d861d7eb50d36cd55ed5e2dd7084a
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2025-03-27 23:48:06 -0600

    agregando puntos

--- controladores/a.js
+++ controladores/a.js
@@ -19,11 +19,11 @@ export const getUser = async (req, res) => {
    };
 
 export const postUser = async (req, res) => {
-    const {username, first_name, last_name, birthdate, password, email} = req.body;
+    const {username, first_name, last_name, birthdate, password, email, points} = req.body;
     const sql = connectDB();
     let query ={
-        text: "INSERT into users (username, first_name, last_name, birthdate, password, email) values ($1, $2, $3, $4, $5, $6)",
-        values: [username, first_name, last_name, birthdate, password, email],
+        text: "INSERT into users (username, first_name, last_name, birthdate, password, email, points) values ($1, $2, $3, $4, $5, $6, $7)",
+        values: [username, first_name, last_name, birthdate, password, email, points],
     }
     console.log(query);
     const data = await sql.query(query);
@@ -31,11 +31,11 @@ export const postUser = async (req, res) => {
    }; 
 
    export const putUser = async (req, res) => {
-    const {user_id, username, first_name, last_name, birthdate, password, email} = req.body;
+    const {user_id, username, first_name, last_name, birthdate, password, email, points} = req.body;
     const sql = connectDB();
     let query ={
-        text: "UPDATE users set username= $2 , first_name= $3 , last_name= $4 , birthdate= $5 , password= $6 , email= $7 WHERE user_id = $1",
-        values: [user_id, username, first_name, last_name, birthdate, password, email],
+        text: "UPDATE users set username= $2 , first_name= $3 , last_name= $4 , birthdate= $5 , password= $6 , email= $7, points=$8 WHERE user_id = $1",
+        values: [user_id, username, first_name, last_name, birthdate, password, email, points],
     }
     console.log(query);
     const data = await sql.query(query);
